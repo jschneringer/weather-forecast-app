d517618474ce7eed6a2e9537e8417a541e4a45eb
Jordan Schneringer <schneringerj@gmail.com>
2022-06-15 23:39:39 -0700

    defining variable, adding event listeners and linking weather api to JS

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var button = document.querySelector('.button');
+var inputValue = document.querySelector('.inputValue');
+var name = document.querySelector ('name');
+var temp = document.querySelector ('temp');
+var desc = document.querySelector ('desc');
+
+
+fetch()
